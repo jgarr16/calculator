53e83395d5a665027b5afa8e1dabdfc4603cbe4d
jrgarrigues <AFK4now@icloud.com>
2016-01-09 23:48:56 -0500

    initial push

---
+++ calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by John Garrigues on 1/9/16.
+//  Copyright © 2016 AFK4now. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
